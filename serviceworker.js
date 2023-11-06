097967d2ca87cf7589f81dd0f1d844d2af6e10b8
Alexander Salomonsson <alexander.salomonsson02@skola.stenungsund.se>
2023-11-06 10:29:53 +0100

    commit
    Jag har skapat en serviceworker

---
+++ serviceworker.js
@@ -0,0 +1,25 @@
+// service-worker.js
+
+const cacheName = 'PWA-APP01';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'style/styles.css',
+        '/main.js',
+        // Lägg till fler resurser som du vill cachas här
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
