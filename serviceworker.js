4624a44ec0f9360a4edc78a6c375c2db1faa6ed6
Alexander Salomonsson <alexander.salomonsson02@skola.stenungsund.se>
2023-11-07 10:06:25 +0100

    commit

--- serviceworker.js
+++ serviceworker.js
@@ -2,31 +2,6 @@
 // Perform install steps
 // serviceworker.js
 
-//const CACHE_NAME = 'my-cache-v1';
-//const urlsToCache = [
-    //'/',
-    //'index.html',
-    //'style/styles.css',
-   // 'main.js'
-//
-
-// self.addEventListener('install', function (event) {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME).then(function (cache) {
-//             console.log('Cache öppnad');
-//             return cache.addAll(urlsToCache);
-//         })
-//     );
-// });
-
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.match(event.request).then(function (response) {
-//             return response || fetch(event.request);
-//         })
-//     );
-// });
-
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('v1')
