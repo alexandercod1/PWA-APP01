7856c30e1d329718e41c6ba25fa5268deca6f3f8
Alexander Salomonsson <alexander.salomonsson02@skola.stenungsund.se>
2023-11-07 08:52:27 +0100

    commit

--- serviceworker.js
+++ serviceworker.js
@@ -1,24 +1,45 @@
 // service-worker.js
 // Perform install steps
-let CACHE_NAME = 'my-cache';
-let urlsToCache = [
-    'style/styles.css',    
-    'main.js',
+// serviceworker.js
+
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+    '/',
     'index.html',
-    'icon/flappy-bird-icon.png'
-    
+    'style/styles.css',
+    'main.js'
+];
 
- 
-    ];
- 
-self.addEventListener('install', function(event) {
-// Perform install steps
+// self.addEventListener('install', function (event) {
+//     event.waitUntil(
+//         caches.open(CACHE_NAME).then(function (cache) {
+//             console.log('Cache öppnad');
+//             return cache.addAll(urlsToCache);
+//         })
+//     );
+// });
+
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith(
+//         caches.match(event.request).then(function (response) {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
+
+
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-        })
-    );
-});
-
+        caches.open('v1')
+            .then(cache => {
+                cache.addAll([
+                    './',
+                    './style/styles.css',
+                    './main.js'
+                ]);
+                console.log('Assets cached')
+                
+            })
+            .catch(err => console.log('could not cache assets')) 
+    )  
+})
